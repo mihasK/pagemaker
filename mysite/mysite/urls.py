86fc9aa9fbc24603083d88b05004d32638082af5
T Kwon <tdkwon@gmail.com>
2015-04-23 13:56:39 -0700

    needed css and js files

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -5,8 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
+    #url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^', include(pagemaker.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
